ef0ed05f33ea5b461d7688d6786b6924d2df89af
Michael van der Westhuizen <r1mikey@gmail.com>
2025-06-18 20:45:15 +0100

    unix: Increase the number of boot memlists
    The current number of boot memlists (1024, 8 pages) handles around
    24GiB of RAM. Increase this number 14336 entries (112 pages, 448KiB),
    tested to handle 512GiB of RAM on Ampere Altra Max.

--- usr/src/uts/armv8/os/fakebop.c
+++ usr/src/uts/armv8/os/fakebop.c
@@ -1613,7 +1613,7 @@ bmemlist_find(struct memlist **listp, uint64_t size, int align)
 static void
 bmemlist_init(struct xboot_info *xbp)
 {
-	static memlist_t boot_list[MMU_PAGESIZE * 8 / sizeof (memlist_t)];
+	static memlist_t boot_list[(MMU_PAGESIZE * 112) / sizeof (memlist_t)];
 	extern struct memlist *phys_install;
 	extern struct memlist *phys_avail;
 	extern struct memlist *boot_allocated;
