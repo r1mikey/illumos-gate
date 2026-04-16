d31cac53292f11758fda762ef9b8fb54316c4318
Michael van der Westhuizen <r1mikey@gmail.com>
2026-04-16 11:37:55 +0100

    platmod: Add support for PCIe RC platform handover
    On ACPI systems we need to call _OSC on the root complex to negotiate
    capabilities and get full control of the hardware from the firmware.
    
    Add a function pointer registration function, to be called from acpidev,
    and a call wrapper, to be called from pcierc.
    
    These are weak functions, and will only be implemented by the SBBR platmod.

--- usr/src/uts/armv8/sys/platmod.h
+++ usr/src/uts/armv8/sys/platmod.h
@@ -21,7 +21,7 @@
 
 /*
  * Copyright 2017 Hayashi Naoyuki
- * Copyright 2025 Michael van der Westhuizen
+ * Copyright 2026 Michael van der Westhuizen
  */
 
 #ifndef _SYS_PLATMOD_H
@@ -54,6 +54,8 @@ struct prom_hwclock;
 #pragma weak	plat_gpio_get
 #pragma weak	plat_gpio_set
 #pragma weak	plat_hwclock_get_rate
+#pragma weak	plat_pcierc_set_takeover
+#pragma weak	plat_pcierc_takeover
 
 /*
  * Called in mp_startup.c from init_cpu_info (twice).
@@ -73,6 +75,29 @@ extern int plat_gpio_set(struct gpio_ctrl *, int);
  * Called in ns16550a.c to get the clock frequency driving the UART.
  */
 extern int plat_hwclock_get_rate(struct prom_hwclock *);
+
+/*
+ * Hooks to allow PCIe root complex drivers to negotiate device takeover
+ * from firmware.
+ *
+ * The firmware implementation uses `plat_pcierc_set_takeover' to install
+ * a takeover function, which receives a dev_info_t pointer and returns
+ * an integer (a standard DDI return code).
+ *
+ * The PCIe root complex uses `plat_pcierc_takeover' at attach time, passing
+ * its dev_info_t pointer. An integer is returned, which is the DDI return
+ * code from the implementation pointer.
+ *
+ * Platform-specific code (in particular, ACPI) implements the firmware
+ * interaction as apropriate.
+ *
+ * These are weak functions, so should be tested for presence prior to
+ * being called.
+ */
+typedef int (*plat_pcierc_takeover_func_t)(dev_info_t *);
+
+extern void plat_pcierc_set_takeover(plat_pcierc_takeover_func_t);
+extern int plat_pcierc_takeover(dev_info_t *);
 
 #endif	/* _KERNEL */
 
